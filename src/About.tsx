b061e1902b28f57667de56b00d4b2cc92d7065f2
Doomfang3 <saubinm@free.fr>
2020-03-31 12:28:05 +0200

    Add install button

--- src/About.tsx
+++ src/About.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const About: React.FC = () => <h1>This is a PWA</h1>;
+const About: React.FC = () => (
+  <>
+    <h1>This is a PWA</h1>
+    <button type="button" id="install-pwa">
+      Install
+    </button>
+  </>
+);
 
 export default About;
